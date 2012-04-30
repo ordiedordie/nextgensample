362de971d6cc8cde654bd12037bfc282e2b33f01
Ordie Dordie <politicalarnold@gmail.com>
2012-04-30 10:45:15 -0700

    added static pages controoller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nextgensample::Application.config.secret_token = 'dfe57282bc1b1d273ed2a680f23618254a2b0a39e9385f19e5b914001eee2388e210b8bf735fa2c03fd7f1895b2cc9d22ae923230cc509382741ea8004cfad03'
